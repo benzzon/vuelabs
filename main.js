03d9b83256791b14a62e6e7dfd0e487cc50cb211
Nils-Olof Bengtsson <nils-olof.bengtsson@mindflower.se>
2021-06-08 16:30:06 +0200

    Upd

--- main.js
+++ main.js
@@ -1,20 +1,25 @@
 var app = new Vue({
     el: '#app',
     data: {
+     brand: 'Vue Mastery',
      product: 'Socks',
-     image: './assets/vmSocks-green-onWhite.jpg',
+     //image: './assets/vmSocks-green-onWhite.jpg',
+     selectedVariant: 0,
+     //inStock: true,
      someRef: 'http://www.benzzon.se',
      onSale: false,
      details: ["80% cotton", "20% polyester", "Gender-neutral"],
      variants: [
        { variantId: 2234,
          variantColor: "green",
-         variantImage: "./assets/vmSocks-green-onWhite.jpg"
+         variantImage: "./assets/vmSocks-green-onWhite.jpg",
+         variantQuantity: 1000
        },
        {
          variantId: 2235,
          variantColor: "blue",
-         variantImage: "./assets/vmSocks-blue-onWhite.jpg"
+         variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+         variantQuantity: 0
        }],
       cart: 0
      },
@@ -22,11 +27,23 @@ var app = new Vue({
        addToCart: function () {
          this.cart += 1
        },
-       updateProduct: function (variantImage) {
-         this.image = variantImage;
+       updateProduct: function (index) {
+         this.selectedVariant = index;
+         // console.log(index);
        },
        removeFromCart: function () {
          this.cart -= 1;
+       }
+     },
+     computed: {
+       title() {
+         return this.brand + ' ' + this.product
+       },
+       image() {
+         return this.variants[this.selectedVariant].variantImage
+       },
+       inStock() {
+        return this.variants[this.selectedVariant].variantQuantity
        }
      }
    })
